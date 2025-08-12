d499398af7b2c29468baa1e287547bfedcc4c34d
Yegor Derevenets <yegor@yegor>
2025-08-12 00:00:00 +0000

    ScreenController

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[forbid(unsafe_code)]
 mod screen;
+mod screen_controller;
 mod switch;
-mod xrandr;
 
 fn main() {
-    let screen = xrandr::query_xrandr();
+    let controller = screen_controller::ScreenController::Xrandr;
+
+    let screen = controller.get_outputs();
     println!("screen = {:?}", screen);
 
-    let mut switch_plan = switch::build_switch_plan(&screen);
+    let switch_plan = switch::build_switch_plan(&screen);
     println!("switch_plan = {:?}", switch_plan);
 
     println!("outputs_to_disable = {:?}", switch_plan.outputs_to_disable.iter().map(|output| output.name.as_str()).collect::<Vec<_>>());
@@ -16,14 +18,5 @@ fn main() {
     let best_resolution = switch::choose_best_resolution(&switch_plan.outputs_to_enable, None);
     println!("best_resolution = {:?}", best_resolution);
 
-    for output in switch_plan.outputs_to_disable {
-        xrandr::disable_output(output);
-    }
-
-    if let Some(first) = switch_plan.outputs_to_enable.pop() {
-        xrandr::enable_output(first, best_resolution, None);
-        for output in switch_plan.outputs_to_enable {
-            xrandr::enable_output(output, best_resolution, Some(first));
-        }
-    }
+    controller.apply(&switch_plan, best_resolution)
 }
